78b8d349d295bce8f6268309fdaf04389927f447
LazuardiMahendra <lazuardhie.mahendra@gmail.com>
2026-04-16 22:33:23 +0800

    init variable from layout

--- CalculatorStoryboard/ViewController.swift
+++ CalculatorStoryboard/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func numberButton(_ sender: Any) {
+    }
+    
+    @IBAction func operationButton(_ sender: Any) {
+    }
+    
 }
 
